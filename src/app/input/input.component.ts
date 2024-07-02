2c7fa42a8cd927cca4bc04b301f02fe2809f4ee8
diegoBuitrago <alejobq@hotmail.com>
2024-07-02 00:11:00 -0500

    change validation and back, get data

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms'
+import { Component} from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,42 +8,86 @@ import { Router } from '@angular/router';
   standalone: true,
   imports: [CommonModule, FormsModule],
   templateUrl: './input.component.html',
-  styleUrl: './input.component.css'
+  styleUrls: ['./input.component.css']
 })
-export class InputComponent{
-
+export class InputComponent {
   public textAllComponent: string;
   public textSelectTypeNumber: string;
   public textNumberDocument: string;
 
   public documentNumber: string;
-  public documentTypeValue: string;
+  public documentTypeValue: string | null;
   
   public documentTypes: DocumentType[];
+  public isButtonDisabled: boolean;
 
-  constructor(public router: Router){
+  public documentNumberError: string;
+
+  constructor(public router: Router) {
     this.textAllComponent = 'Todos los campos son obligatorios';
     this.textSelectTypeNumber = 'Tipo de Documento';
     this.textNumberDocument = 'Número de Documento';
     
     this.documentNumber = '';
-    this.documentTypeValue = 'D';
+    this.documentTypeValue = null;
     
     this.documentTypes = [
       { id: 'C', name: 'Cédula de Ciudadanía' },
       { id: 'P', name: 'Pasaporte' }
     ];
+
+    this.isButtonDisabled = false;
+    this.documentNumberError = '';
   }
 
   public search(): void {
+    this.documentNumberError = '';
+    this.documentNumber = this.removeCommas(this.documentNumber);
+    
+    if (!this.validateDocumentNumber(this.documentNumber)) {
+      return;
+    }
+
     console.log('Número de Documento:', this.documentNumber);
     console.log('Tipo de Documento:', this.documentTypeValue);
     this.router.navigate(['/response'], {
-       state: {
-         documentTypeValue: this.documentTypeValue, 
-         documentNumber: this.documentNumber }
-        }
-    );
+      state: {
+        documentTypeValue: this.documentTypeValue, 
+        documentNumber: this.documentNumber 
+      }
+    });
+  }
+
+  private validateDocumentNumber(number: string): boolean {
+    if(!this.validateIsNumber(number)) {
+      this.documentNumberError = 'El número de documento solo puede contener números.';
+      return false;
+    }
+    if (number.length < 8 || number.length > 11) {
+      this.documentNumberError = 'El número de documento debe tener entre 8 y 11 caracteres.';
+      return false;
+    }
+    this.documentNumberError = '';
+    return true;
+  }
+
+  private validateIsNumber(number: string): boolean {
+    let numberWithoutCommas = this.removeCommas(number);
+    let parsedNumber = parseFloat(numberWithoutCommas);
+    if (isNaN(parsedNumber)) {
+      return false;
+    }
+    return true;
+  }
+
+  public formatNumber(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    let value = input.value.replace(/\D/g, '');
+    this.documentNumber = value.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  }
+
+  public removeCommas(str: string): string {
+    return str.replace(/,/g, '');
   }
 }
 
--- src/app/response/response.component.ts
+++ src/app/response/response.component.ts
@@ -12,13 +12,46 @@ import { HttpClient } from '@angular/common/http';
 })
 
 export class ResponseComponent implements OnInit{
-  public clientInfo: any;
-  public clientDetails: any;
+  public clientInfo: any
+  public clientDetails: any
+
+  public textFirstLastName: string
+  public textFirstName: string
+  public textFirstSecondLastName: string
+  public textMiddleName: string
+  public textPhone: string
+  public textCity: string
+  public textAdress: string
+
+  public valueFirstLastName: string
+  public valueFirstName: string
+  public valueFirstSecondLastName: string
+  public valueMiddleName: string
+  public valuePhone: string
+  public valueCity: string
+  public valueAdress: string
 
   constructor(private router: Router, private clientService: ClientService) {
-    const navigation = this.router.getCurrentNavigation();
-    this.clientInfo = navigation?.extras.state;
+    const navigation = this.router.getCurrentNavigation()
+    this.clientInfo = navigation?.extras.state
+
+    this.textFirstLastName = 'Primer Apellido'
+    this.textFirstName = 'Primer Nombre'
+    this.textFirstSecondLastName = 'Segundo Apellido'
+    this.textMiddleName = 'Segundo Nombre'
+    this.textPhone = 'Teléfono'
+    this.textCity = 'Ciudad'
+    this.textAdress = 'Dirección'
+
+    this.valueFirstLastName = ''
+    this.valueFirstName = ''
+    this.valueFirstSecondLastName = ''
+    this.valueMiddleName = ''
+    this.valuePhone = ''
+    this.valueCity = ''
+    this.valueAdress = ''
   }
+
   ngOnInit(): void {
     console.log(this.clientInfo)
 
@@ -27,6 +60,8 @@ export class ResponseComponent implements OnInit{
         .subscribe(
           data => {
             this.clientDetails = data;
+            console.log(this.clientDetails.primer_nombre)
+            this.getDataByService();
             console.log(this.clientDetails);
           },
           error => {
@@ -34,5 +69,22 @@ export class ResponseComponent implements OnInit{
           }
         );
     }
+  }
+
+  private getDataByService(): void{
+    this.valueFirstLastName = this.clientDetails.primer_apellido
+    this.valueFirstName = this.clientDetails.primer_nombre
+    this.valueFirstSecondLastName = this.clientDetails.segundo_apellido
+    this.valueMiddleName = this.clientDetails.segundo_nombre
+    this.valuePhone = this.clientDetails.telefono
+    this.valueCity = this.clientDetails.ciudad
+    this.valueAdress = this.clientDetails.direccion
+  }
+
+  public redirectToInput(): void {
+    this.router.navigate(['/input'], {
+      state: {
+      }
+    });
   }
 }
